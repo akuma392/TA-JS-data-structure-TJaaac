5b06032059f4a95370ec4357f0d296a42277eb3b
Abhishek Kumar <abhivision3@gmail.com>
2020-11-08 18:52:07 +0530

    1st commit

--- block-BJaaca/code/index.js
+++ block-BJaaca/code/index.js
@@ -2,23 +2,35 @@
 
 //  - Create a variable named `character` using `let` assign a value of empty object.
 
+let character = {};
+
 //  - Go to this [Arya Stark](https://awoiaf.westeros.org/index.php/Arya_Stark)
 
 //  Re-assign the value of `character` variable to and object with the key `characterName` and value of "Arya"
+character.characterName = "Arya"
 
 // - A variable named `age` is predefined with value 20. Add a new key named `character-age` with the value of `age` variable i.e 20 (don't use the value 20)
 
 let age = 20;
+character["character-age"]= age;
+
 
 // - Add a new key `sur name` with the value of the surname of the character i.e "Stark". (Don't remove the space between `sur` and `name`)
+ 
+character["sur name"] = "Stark"
 
 // - Add a new key `title` with the value title of the character i.e "Lady of Winterfell"
+character.title = "Lady of Winterfall";
 
 // - Add another property named `greet` and value should be a function when called should alert `I am [NAME HERE] and my title is [TITLE HERE]`.
 
+// character["greet"] = `I am ${characterName} ${'sur name'} and my title is ${title}`;
+
 // - Add a new property to the character object named `isFemale` and value will be `true`
 
+character.isFemale = true;
 // - Execute the function stored in the greet key of the character object
+
 
 //  - Change the method `greet` to now alert `She is [NAME HERE] and her title is [TITLE HERE]`.
 
@@ -33,9 +45,11 @@ let age = 20;
 let keyName = 'playedBy';
 
 // - Using `alert` alert the value stored in key you added above `keyName` use the variable name to access
-
+alert(keyName);
 // - Using the function `console.log` log the value of the key `42`. You don't have to add the key
 
 // - Add a new key named `totalSeasons` with the value of `1 + 2 + 3 + 2`. Use this expression
 
 // - Access the value of the key `totalSeason` (it should be 8)
+
+
